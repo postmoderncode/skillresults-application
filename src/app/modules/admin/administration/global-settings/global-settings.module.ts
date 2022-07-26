b2d9bc1a7c120526d1a6efc153a1693d5d2cd34b
bmlewandowski <bmlewandowski@gmail.com>
2022-07-25 22:35:02 -0400

    SkillResults-Application: added mat-slide-toggles to Global Settings

--- src/app/modules/admin/administration/global-settings/global-settings.module.ts
+++ src/app/modules/admin/administration/global-settings/global-settings.module.ts
@@ -3,12 +3,20 @@ import { CommonModule } from '@angular/common';
 import { GlobalSettingsComponent } from './global-settings.component';
 import { Route, RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 
 const globalsettingsRoutes: Route[] = [
-    {
-        path     : '',
-        component: GlobalSettingsComponent
-    }
+  {
+    path: '',
+    component: GlobalSettingsComponent
+  }
 ];
 
 @NgModule({
@@ -18,6 +26,14 @@ const globalsettingsRoutes: Route[] = [
   imports: [
     CommonModule,
     MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatTooltipModule,
     RouterModule.forChild(globalsettingsRoutes)
   ]
 })
--- src/app/modules/admin/reports/my-reports/my-reports.component.ts
+++ src/app/modules/admin/reports/my-reports/my-reports.component.ts
@@ -83,5 +83,3 @@ export class MyReportsComponent implements OnInit, OnDestroy {
 
 
 }
-
-
