0f05415e6df5d98121133cfe884087c8ffed3dd2
bmlewandowski <bmlewandowski@gmail.com>
2022-07-31 00:43:08 -0400

    SkillResults-Application: removed console.log messages in public-profile

--- src/app/modules/admin/home/public-profile/public-profile.component.ts
+++ src/app/modules/admin/home/public-profile/public-profile.component.ts
@@ -58,7 +58,6 @@ export class PublicProfileComponent implements OnInit, OnDestroy {
     this.db.object('/users/' + this.id)
       .valueChanges().subscribe(
         (results: any[]) => {
-          console.log(results);
           this.user = results;
         }
 
