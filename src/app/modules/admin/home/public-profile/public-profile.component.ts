fe1c5bcb8960675023095917485ed2c70f751015
bmlewandowski <bmlewandowski@gmail.com>
2022-07-31 06:24:40 -0400

    SkillResults-Application: replace missing div tag on public-profile

--- src/app/modules/admin/home/public-profile/public-profile.component.ts
+++ src/app/modules/admin/home/public-profile/public-profile.component.ts
@@ -58,6 +58,7 @@ export class PublicProfileComponent implements OnInit, OnDestroy {
     this.db.object('/users/' + this.id)
       .valueChanges().subscribe(
         (results: any[]) => {
+          console.log(results)
           this.user = results;
         }
 
